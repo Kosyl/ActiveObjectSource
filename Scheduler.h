054c11be264a0b2327a58fdf80e4896358b51bda
Marta <marta.kuzak@gmail.com>
2013-05-21 23:28:55 +0200

    Zaczynamy transformację planisty.
    Generacja ruchu. Dobranoc

--- Scheduler.h
+++ Scheduler.h
@@ -3,8 +3,8 @@
 
 #include "ActivationQueue.h"
 #include "Command.hpp"
+#include "Proxy.hpp"
 #include <boost/thread/mutex.hpp>
-#include <boost/thread/detail/thread_group.hpp>
 #include <boost/smart_ptr/shared_ptr.hpp>
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
@@ -20,51 +20,52 @@ class Scheduler
 	typedef boost::shared_ptr<Funktor> FunPtr;
 public:
 	Scheduler(void) {}
-	/**
-	* @param n - number of threads
+	/*
+	* @param q pointer to queue
 	*/
-	Scheduler(int n) : n_(n), alive(true) {
-		boost::lock_guard<boost::mutex> guard(mutex_);
-        for(int i = 0; i < n_; ++i) {     
-			std::auto_ptr<boost::thread> tmp_thread(new boost::thread(boost::bind(&Scheduler::run,this)));
-            threads_.add_thread(tmp_thread.get());
-			tmp_thread.release();
-        }
-		threads_.join_all();
-    }
-
-	~Scheduler(void) {
-		alive= false;
-		threads_.interrupt_all(); // czy to zgrabny sposob?
+	Scheduler(ActivationQueue* q): queue_(q){
+		thread_=boost::thread(boost::bind(&Scheduler::run,this));
 	}
+
+
+	~Scheduler(void) {}
 	void enqueue(FunPtr f) {
 		boost::mutex::scoped_lock lock(mutex_);
-		queue_.push(f);
+		queue_->push(f);
 	}
+
+	//void notify
 	
 	
 private:
 	void dequeue()  {
-		while(!queue_.empty()){
+		while(!queue_->empty()){
 			boost::mutex::scoped_lock lock(mutex_);
-			queue_.pop()->execute();
+			FunPtr fun= queue_->pop();
+			if(fun) fun->execute();
+			else break;
 		}
 	}
 	void run() { //nie wiem, czy to trafiana nazwa, chodzi o funkcje, ktora ma leciec przez caly cykl zycia Schedulera
-		while(alive){
-			while(!queue_.empty()){
-				boost::lock_guard<boost::mutex> guard(mutex_);
+		while(true){ //jakis tam warunek
+			//while(!queue_->empty()){
+				//boost::lock_guard<boost::mutex> guard(mutex_);// w dequeue sa locki 
 				dequeue();
-			}
+			//}
 		}
 	}
 
+	bool stop() { //zatrzymywanie watku
+		thread_.join();
+		//jakas flaga na false?
+		return true;
+	}
 
-	ActivationQueue queue_;
+	
+	ActivationQueue* queue_;
+	Proxy* proxy_; //chyba nie
+	boost::thread thread_;
 	mutable boost::mutex mutex_;
-	boost::thread_group threads_;
-	int n_;
-	bool alive; //znowu ta sama uwaga co przy run() :D
 };
 
 //Scheduler::
