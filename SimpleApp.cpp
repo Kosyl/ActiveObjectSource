d46a50d8037cc14edd49012cc879b2e46c899263
Marta <marta.kuzak@gmail.com>
2013-06-04 23:50:53 +0200

    ''''

--- SimpleApp.cpp
+++ SimpleApp.cpp
@@ -1,3 +1,9 @@
+/**
+* @file SimpleApp.cpp
+* @author Michal Kosyl
+* @author Marta Kuzak
+* @details Active Object simple aplication test. 
+*/
 #include "SimpleLog.hpp"
 #include "FutureContentCreator.hpp"
 #include <boost/function.hpp>
@@ -31,17 +37,17 @@ public:
 		boost::this_thread::sleep(boost::posix_time::milliseconds(3000));
 		{
 			boost::mutex::scoped_lock l(mutex_);
-			cout<<"\""<<word<<",taaaaaak..."<<endl;
+			cout<<"'"<<word<<",taaaaaak...'"<<endl;
 		}
 		boost::this_thread::sleep(boost::posix_time::milliseconds(2000));
 		{
 			boost::mutex::scoped_lock l(mutex_);
-			cout<<"'Hmmm, "<<word<<", "<<word<<", "<<word<<", ile lat jeszcze pozyjesz?"<<endl;
+			cout<<"'Hmmm, "<<word<<", "<<word<<", "<<word<<", ile lat jeszcze pozyjesz?'"<<endl;
 		}
 		boost::this_thread::sleep(boost::posix_time::milliseconds(2500));
 		{
 			boost::mutex::scoped_lock l(mutex_);
-			cout<<"'Taaaak, to bardzo ciekawe... "<<word<<", zaraz poznasz swoja przyszlosc!"<<endl;
+			cout<<"'Taaaak, to bardzo ciekawe... "<<word<<", zaraz poznasz swoja przyszlosc!'"<<endl;
 		}
 		boost::this_thread::sleep(boost::posix_time::milliseconds(3000));
 		int v = rand() % 20 +5;
@@ -77,7 +83,6 @@ public:
 
 int main(int argc, char* argv[])
 {	
-	int tmp=1;
 	cout<<"Wpisz imie osoby, ktorej przyszlosc chcesz poznac. Wrozbita Maciej powie ci najwieksze sekrety wiecznosci!"<<endl;
 	cout<<"Wrozbita Maciej moze przewidywac przyszlosc dla 3 osob jednoczesnie!"<<endl;
 	cout<<"Wpisz 'Maciej', zeby sploszyc wrozbite!"<<endl;
